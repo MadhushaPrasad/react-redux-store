5f071ab7f09853a46ebe403953c403398c230150
Madhusha Prasad <madushaprasad21@gmail.com>
2025-09-27 15:31:13 +0530

    feat: product component done

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
 
 function Products() {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data)
+        console.log(data)
+      })
+  }, [])
+
+  const cards = products.map((product) => {
+    return (
+      <div className="col-md-3" style={{marginBottom:'10px'}}>
+        <Card key={product.id} className='h-100'>
+          <div className='text-center my-3'>
+            <Card.Img variant="top" src={product.image} style={{ width: '100px', height:'130px' }}/>
+          </div>
+          <Card.Body>
+            <Card.Title>{product.title}</Card.Title>
+            <Card.Text>LKR: {product.price}</Card.Text>
+          </Card.Body>
+          <Card.Footer style={{background:'white'}}>
+            <Button variant="primary">Add to cart</Button>
+          </Card.Footer>
+        </Card>
+      </div>
+    )
+  })
+
   return (
-    <div>
-      Product Dashboard
-    </div>
+    <>
+      <h1 className="mt-5 mb-5">Product Dashboard</h1>
+      <div className="container row w-100">{cards}</div>
+    </>
   )
 }
 
