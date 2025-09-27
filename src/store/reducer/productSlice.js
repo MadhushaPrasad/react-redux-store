f65540834f113cbed419a882934bf6746b13f15b
Madhusha Prasad <madushaprasad21@gmail.com>
2025-09-27 18:41:54 +0530

    feat: create product slice pro manage product state

---
+++ src/store/reducer/productSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = []
+
+const productSlice = createSlice({
+  name: 'product',
+  initialState: initialState,
+  reducers: {
+   
+  },
+})
+
+export default productSlice.reducer
