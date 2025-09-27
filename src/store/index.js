9a92d5f81846a1a8caea960432ccc0f31b16c8b1
Madhusha Prasad <madushaprasad21@gmail.com>
2025-09-27 18:52:01 +0530

    chore: setup productSlice to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cartSlice from './reducer/cartSlice'
+import productSlice from './reducer/productSlice'
 
 const store = configureStore({
   reducer: {
     cart:cartSlice,
+    product:productSlice
   },
 })
 
