2c52dffe97c6e6aefacf4e326bc7d0fd829e740c
Madhusha Prasad <madushaprasad21@gmail.com>
2025-09-27 18:21:49 +0530

    chore: display cart product count

--- src/components/NavBarPanel.js
+++ src/components/NavBarPanel.js
@@ -1,9 +1,13 @@
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 function NavBarPanel() {
+
+  const cartProduct =useSelector(state => state.cart );
+
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid>
@@ -18,7 +22,7 @@ function NavBarPanel() {
         <Navbar.Collapse className="justify-content-end">
           <Navbar.Text>
             <Nav.Link to="/cart" as={Link}>
-              My bag 0
+              My bag {cartProduct.length}
             </Nav.Link>
           </Navbar.Text>
         </Navbar.Collapse>
